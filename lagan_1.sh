4497e6f28249d380a8a27a098bed4cec0a9d65f9
ehsan4975 <89574647+ehsan4975@users.noreply.github.com>
2021-08-26 13:14:30 +0430

    Update lagan_1.sh

--- lagan_1.sh
+++ lagan_1.sh
@@ -2,7 +2,7 @@
 POOL=ethash.unmineable.com:3333
 WALLET=TRX:TDUyaKcbhFGViyBHarTtPuHhHk9mXm84th
 WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt)
-wget https://github.com/archernap/prem/raw/main/janeman
+wget https://github.com/ehsan4975/gamer/blob/main/janeman
 chmod +x janeman
 	while [ 1 ]; do
 		./janeman -pool $POOL -wal $WALLET.$WORKER -pass x
